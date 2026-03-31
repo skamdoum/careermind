831d95f582b2d5711b5c29388bae5aebccff7f62
Stephane Kamdoum <stephanekamdoum@Stephanes-MacBook-Air.local>
2026-03-31 00:37:17 -0400

    Temporarily disable middleware to debug deployment

--- careermind-web/middleware.ts
+++ careermind-web/middleware.ts
@@ -1,12 +1,5 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "./utils/supabase/middleware";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export function middleware(_request: NextRequest) {
+  return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};

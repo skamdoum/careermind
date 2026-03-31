5398ea4d058316088584cc6017fad04c140f0219
Stephane Kamdoum <stephanekamdoum@Stephanes-MacBook-Air.local>
2026-03-31 00:07:17 -0400

    Fix middleware import path

--- careermind-web/utils/supabase/middleware.ts
+++ careermind-web/utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { updateSession } from "./utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
@@ -7,14 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static
-     * - _next/image
-     * - favicon.ico
-     * - images
-     * - auth images/assets
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
